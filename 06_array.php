5acd31e96ef009004f8e3d2439014abb66f542eb
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-04-27 20:24:14 -0300

    Finalizando a aula 6 e inicio 7

--- 06_array.php
+++ 06_array.php
@@ -13,9 +13,60 @@ $carroUm = [
 
 echo $carroUm[ 'cor']; 
 echo "<br>";
-echo $carroUm['modelo'];"<br>";
+echo $carroUm['modelo'];
 echo '<br>';
-echo $carroUm['marca'];"<br>";
+echo $carroUm['marca'];
+echo ' - ';
+echo $carroUm['marca'];
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+
+$carroDois = [
+    'modelo' => 'punto',
+    'marca' => 'fiat',
+    'cor' => 'amarelo'
+];
+
+echo $carroDois ['modelo'];
+echo '<br>';
+echo $carroDois ['marca'];
+echo'<br>';
+echo $carroDois ['cor'];
+echo '<br>';
+
+echo '<br>'; echo '<br>';
+
+
+
+$carroTresTheer = [
+    'modelo' => 'S10',
+    'marca' => 'Fiat',
+    'cor' => 'black',
+];
+
+echo $carroTresTheer ['modelo'];
+echo '<br>';
+echo $carroTresTheer ['marca'];
+echo '<br>';
+echo $carroTresTheer ['cor'];
+echo '<br>'; echo '<br>';
+
+
+$carroQuatroFour = [
+    'modelo' => 'Toro',
+    'marca' =>  'Ford',
+    'cor' => 'Blue'
+];
+
+echo 'Modelo: '; echo $carroQuatroFour ['modelo']; echo'<br>';
+echo 'Marca: '; echo $carroQuatroFour ['marca']; echo '<br>';
+echo 'Cor: '; echo $carroQuatroFour ['cor']; echo '<br>';
+
+
+
+
 
 
 ?>
---
+++ 07_manipulacao_arrays.php
@@ -0,0 +1,17 @@
+<?php
+
+$inforEmpresa = [
+    'nome' => 'Senac Amercaian',
+    'funcionarioi' => 'Paulo SAntos',
+    'anoAtual' => 2026,
+    ];
+
+    echo $inforEmpresa ['nome'];
+   
+
+    $infoCursos = [
+        'curso' => ['pho','js','VueJs','Laravel'],
+        'totalCurso' => 26,
+    ];
+
+    ?>
