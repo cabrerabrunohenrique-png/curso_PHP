fab6d842d351d0b7cc1f12cabdbf1a7341a29a5d
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-04-27 21:25:14 -0300

    Update 07_manipulacao_arrays.php

--- 07_manipulacao_arrays.php
+++ 07_manipulacao_arrays.php
@@ -14,4 +14,21 @@ $inforEmpresa = [
         'totalCurso' => 26,
     ];
 
+    echo '<pre>';
+
+    /* o ARRAY KEYS vai informar as chaves do ARRAY e nao o valor;
+    ou seja o TITUlo e nao o valor*/ 
+    var_dump(array_keys($inforEmpresa));
+
+    /* o ARRAY VALUES vai  o valor*/ 
+    var_dump(array_values($inforEmpresa));
+    /* informa a quantidade de colunas dentro de um ARRAY*/
+    var_dump(count($inforEmpresa));
+
+    $inforEmpresa = array_merge($inforEmpresa,$infoCursos);
+    print_r($inforEmpresa);
+
+
+    
+
     ?>
