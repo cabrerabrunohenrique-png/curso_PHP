6168468610505f2dc011d66e7e5ce0aeef7f4987
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-04 20:20:59 -0300

    Usando o IF

---
+++ 13_estrutura_condicional_if.php
@@ -0,0 +1,15 @@
+<?php
+
+$numero = 12;
+$numeroDOis = 12;
+
+if ($numero ==$numeroDOis){
+    echo 'é igual';
+    echo ' outro algo';
+}
+
+
+
+
+
+?>
---
+++ 13_operadores.php
@@ -0,0 +1,19 @@
+*/Operadores Aritméticos
++ soma
+- subtrai
+/ divide
+* multiplica
+% resto
+
+Operadores Relacionais
+> maior
+< menor
+>= maior igual
+<= menor igual
+== igual
+!= diferente
+
+Operadores lógicos
+&& e
+|| ou
+!  não*/
