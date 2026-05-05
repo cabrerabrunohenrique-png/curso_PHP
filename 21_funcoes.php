fc45a10a2b6bb924ebb37c9c587cef3fc74a1c6a
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-05 20:19:02 -0300

    base para criar funcoes

--- 21_funcoes.php
+++ 21_funcoes.php
@@ -4,6 +4,29 @@ function escrever($texto){
     echo "{$texto} <br>";
 }
 
-escrever('algo')
+escrever("algo");
+
+
+
+function soma($n1, $n2){
+    return $n1 + $n2;
+}
+
+echo soma(12,90).'<br>';
+
+
+function conta ($n1, $n2, $taxa =2){
+    return ($n1 + $n2) * $taxa;
+
+}
+echo conta(1,2) .'<br>';
+
+
+function media($n1,$n2,$n3,$n4){
+    return($n1+$n2+$n3+$n4) / 4;
+
+}
+echo  'Sua média é ' . media(2,5,8,10);
+
 
 ?>
