2d22df457e842adcc47f016ac259a4731979e054
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-04 22:09:37 -0300

    repeticao para lista
    repeticao para lista

---
+++ 17_loops_repeticao_foreach.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = [
+    'aluno1' => 'erica',
+    'aluno2' => 'ana',
+    'aluno3' => 'igor',
+    'aluno4' => 'leandro',
+];
+
+// esse comando é mais simples para leitura de lista
+// o foreach vai precisar do "Array" nesse caso é o $nome
+// depois ele vai pedir o nome das variaves para distrubuir os dados
+// no caso "aluno e erica"
+
+foreach ($nomes as $chave => $nome)
+    {
+        echo"{$nome} <br>";
+    }
+
+
+?>
