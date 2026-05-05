5ed4d876ac0ed3903765ae3db9d83a3e8e7e97b4
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-04 21:35:06 -0300

    loop de repeticao

--- 16_loops_repeticao_for.php
+++ 16_loops_repeticao_for.php
@@ -1,6 +1,26 @@
 <?php
 
+$nomes = [
+    'joao',
+    'rita',
+    'claudio',
+    'fernanda'
+    ];
 
+    echo '<hr>';
 
+ // essa é a estrutura de loope de repeticao
+ // primeiro vc informa o inicio
+ // segundo a condicao
+ // terceira o encremendo 
+
+ // primeiro( começar no 0)
+ //segundo ( enquanto o conut( nesse caso é um comando que conta a
+ // cas do array))
+ // terceiro ele vai somar 
+
+    for ($i = 0; $i <count($nomes); $i++){
+            echo $nomes [$i] .'<br>' ;
+    }
 
 ?>
