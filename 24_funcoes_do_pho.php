9f39afda7e32e620ca7e5cbc080d8c37dbe923f6
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-05 21:32:56 -0300

    ele vai retirar o espaço do lado DIREITO

--- 24_funcoes_do_pho.php
+++ 24_funcoes_do_pho.php
@@ -5,7 +5,13 @@ echo '<pre>';
 
 $nome = ' senac americana ';
 var_dump($nome);
-var_dump(ltrim($nome));
+var_dump(ltrim($nome)) .'<br>';
+
+
+$nome1 = ' senac americana ';
+var_dump($nome1);
+var_dump(rtrim($nome1)) .'<br>';
+
 
 
 
