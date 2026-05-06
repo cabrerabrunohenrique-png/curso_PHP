efed99f102c34455fe104e9f0ced42a505f844be
cabrerabrunohenrique-png <cabrerabrunohenrique@gmail.com>
2026-05-05 22:16:21 -0300

    Tras os hoarios que pediu

--- 26_funcoes_do_php.php
+++ 26_funcoes_do_php.php
@@ -3,12 +3,21 @@
 $nome ='carlos';
 
 
-    // funcao para validar se existe a variavel
+    // funcao para validar se existe a variavel( no caso saida é verdadeira)
+if (isset($nome)){
+    echo $nome. '<hr>';
+
+}else{
+    echo 'nao exixte'. '<hr>';
+}
+
+//funcao para "matar" a variavel
+unset ($nome);
 if (isset($nome)){
     echo $nome;
 
 }else{
-    echo 'nao exixte';
+    echo 'nao existe';
 }
 
 
---
+++ 27_funcoes_do_php.php
@@ -0,0 +1,19 @@
+<?php
+
+echo '<pre>';
+
+
+// ele tras o horario que vc deseja para "validacao"
+date_default_timezone_set('America/Sao_Paulo');
+echo 'o ano atul:' . date('Y') . '<br>';
+echo 'o mes atul:' . date('m') . '<br>';
+echo 'o dia atul:' . date('d') . '<br>';
+echo 'A data atual:' . date('d/m/y') . '<br>';
+echo 'Hora atual' . date('H/i/s') . '<br>';
+
+
+
+
+
+
+?>
